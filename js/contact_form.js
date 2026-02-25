92cd6e9b898c6880cdce3b9692ac0a3d9c122a1c
Briana Maria Iaru <briana.iaru@student.unitbv.ro>
2026-02-25 12:40:06 +0200

    Lab 1: HTML and CSS review

---
+++ js/contact_form.js
@@ -0,0 +1,12 @@
+function submitForm() {
+
+    const nume = document.getElementById("name").value;
+    const email = document.getElementById("email").value;
+    const mesaj = document.getElementById("message").value;
+
+    console.log("Nume utilizator:", nume);
+    console.log("Email utilizator:", email);
+    console.log("Mesaj:", mesaj);
+
+    console.warn("Goodbye World!");
+}
