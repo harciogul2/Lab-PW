2fbd82db5ab925b5457388b72aa07be2853aba44
Briana Maria Iaru <briana.iaru@student.unitbv.ro>
2026-03-04 12:52:06 +0200

    aduagare validare la formular

--- js/contact_form.js
+++ js/contact_form.js
@@ -8,5 +8,28 @@ function submitForm() {
     console.log("Email utilizator:", email);
     console.log("Mesaj:", mesaj);
 
+    // Validari
+        if (nume.length < 2) {
+            feedbackMsg.textContent = "Eroare: Numele trebuie să aibă cel puțin 2 caractere!";
+            feedbackMsg.style.color = "red";
+            return;
+        }
+
+        if (!email.includes('@')) {
+            feedbackMsg.textContent = "Eroare: Adresa de email trebuie să conțină caracterul '@'!";
+            feedbackMsg.style.color = "red";
+            return;
+        }
+
+        if (mesaj.length < 10) {
+            feedbackMsg.textContent = "Eroare: Mesajul trebuie să conțină cel puțin 10 caractere!";
+            feedbackMsg.style.color = "red";
+            return;
+        }
+
+        // totul ok
+        feedbackMsg.textContent = `Mulțumim, ${nume}! Mesajul a fost trimis.`;
+        feedbackMsg.style.color = "green";
+        
     console.warn("Goodbye World!");
 }
