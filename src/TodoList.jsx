610910e0faa05fef9ba1c50bfb67586e5f2da737
Briana Maria Iaru <briana.iaru@student.unitbv.ro>
2026-03-25 13:24:20 +0200

    adaugare buton stergere task

--- src/TodoList.jsx
+++ src/TodoList.jsx
@@ -1,31 +1,62 @@
 import { useState } from 'react';
+
 function TodoList() {
- const [todos, setTodos] = useState([]);
- const [input, setInput] = useState('');
- function handleAdd() {
- if (input.trim() === '') return; // Nu adauga text gol
- setTodos([...todos, input]); // Creeaza array NOU cu tot ce era + input
- setInput(''); // Goleste input-ul
- }
- return (
- <div>
- <h3>Todo List</h3>
- <input
- value={input}
- onChange={(e) => setInput(e.target.value)}
- placeholder="Adauga un task..."
- />
- <button onClick={handleAdd}>Adauga</button>
- {/* TODO: Afisati lista de todos cu map() */
-    
-        <ul>
+  const [todos, setTodos] = useState([]); // array gol la inceput
+  const [input, setInput] = useState(''); // text gol la inceput
+
+  function handleAdd() {
+    if (input.trim() === '') return; // nu adauga daca e doar spatiu gol
+    setTodos([...todos, input]); // creeaza un array NOU: pune ce era inainte + noul input
+    setInput(''); // goleste casuta dupa ce ai adaugat
+  }
+
+
+  //  functie->index task pe care am dat click
+  function handleDelete(index) {
+    // filter()-> array vechi=>>>array nou complet
+    // _ -> textul task-ului- underscore = nu ne foloseste variabila.
+    // i -> reprezinta indexul fiecarui task
+    // i !== index-> pastreaza elem mai putin pe crl pe care am dat click 
+    setTodos(todos.filter(function(_, i) {
+      return i !== index;
+    }));
+  }
+
+  return (
+    <div style={{ padding: '20px', border: '1px solid #ccc', borderRadius: '10px', marginBottom: '20px' }}>
+      <h3>Todo list</h3>
+      
+      <input
+        value={input}
+        onChange={(e) => setInput(e.target.value)}
+        placeholder="adauga un task..."
+        style={{ padding: '8px', marginRight: '10px' }}
+      />
+      <button onClick={handleAdd} style={{ padding: '8px 15px' }}>adauga</button>
+
+      <ul>
         {todos.map(function(todo, index) {
-          return <li key={index}>{todo}</li>;
+          //buton in lista
+          return (
+            <li key={index}>
+              {}
+              {todo} 
+              
+              {/*butonul de stergere 
+                onClick={() => handleDelete(index)}: 
+                () => -> nu sterge totul cand se incarca pagina doar cand se fcae click*/}
+              <button 
+                onClick={() => handleDelete(index)} 
+                style={{ marginLeft: '10px' }}
+              >
+                Sterge
+              </button>
+            </li>
+          );
         })}
       </ul>
-   
- }
- </div>
- );
+    </div>
+  );
 }
+
 export default TodoList;
