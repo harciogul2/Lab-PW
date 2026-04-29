96074febec39e29d22388a3d13d5f0ea4c728a9e
Briana Maria Iaru <briana.iaru@student.unitbv.ro>
2026-04-29 13:25:55 +0300

    lab8-ex4-adaugare proiect-POST

--- server/index.js
+++ server/index.js
@@ -2,11 +2,15 @@ const express = require('express');
 const app = express();
 const PORT = 3000;
 
+//lab8-ex 4
+// Middleware pentru citirea JSON din body
+app.use(express.json()); 
+
 // GET / - ruta principala Rută API pentru proiecte
 app.get('/', function(req, res) {
   res.json({ message: 'Serverul functioneaza!' });
 });
-
+//lab 8 ex 2
 // Date (temporar in memorie,vom folosi MongoDB mai tarziu)
 const projects = [
   { id: 1, title: "Pagina Personala", tech: "HTML, CSS", done: true },
@@ -20,6 +24,7 @@ app.get('/api/projects', function(req, res) {
   res.json(projects);
 });
 
+//lab8 ex 3
 //GET /api/projects/:id -returneaza un singur proiect dupa id
 app.get('/api/projects/:id', function(req, res) {
   const project = projects.find(p => p.id === parseInt(req.params.id));
@@ -35,6 +40,19 @@ app.get('/api/stats', function(req, res) {
     finalizate: projects.filter(p => p.done).length,
     inLucru: projects.filter(p => !p.done).length,
   });
+});
+
+//lab8 ex 4
+// POST /api/projects - adauga un proiect nou
+app.post('/api/projects', function(req, res) {
+  const newProject = {
+    id: projects.length + 1,
+    title: req.body.title,
+    tech: req.body.tech,
+    done: req.body.done || false,
+  };
+  projects.push(newProject);
+  res.status(201).json(newProject);
 });
 
 // Porneste serverul
